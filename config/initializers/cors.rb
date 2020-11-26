1b61a5daaf21448fbffe5253966fafa2823693cb
teekaytech <taofeek.olalere@feghas.com>
2020-11-26 22:12:11 +0100

    Upudate cors origins and session url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://my_auth_frontend_react_app.herokuapp.com'
+    origins 'https://react-frontend-api-check.netlify.app/'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
-if test
-  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'my_rails_backend_app_api.herokuapp.com'
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'https://glacial-wave-59879.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
