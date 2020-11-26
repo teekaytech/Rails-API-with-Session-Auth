aa35c9c828a6770270a4538be778b2f0eb8610fe
teekaytech <taofeek.olalere@feghas.com>
2020-11-26 11:36:57 +0100

    Setup models and controllers for user registration and login

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'my_rails_backend_app_api.herokuapp.com'
-
-# put the api domain here
+if test
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'my_rails_backend_app_api.herokuapp.com'
+else
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app'
+end
