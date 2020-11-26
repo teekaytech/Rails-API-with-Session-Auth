2cfcd80c7976470b8bc9f40a37063eafbeb0958c
teekaytech <taofeek.olalere@feghas.com>
2020-11-26 23:54:23 +0100

    Update cors.rb file\

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,4 +8,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'https://react-frontend-api-check.netlify.app/'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
+
+  allow do
+    origins '*'
+    resource '/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 end
