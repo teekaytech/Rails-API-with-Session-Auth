73498da1241c9e37a655972dc29d8fbee10919cc
teekaytech <taofeek.olalere@feghas.com>
2020-11-26 22:25:40 +0100

    Update session store to solve cors policy error

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'https://glacial-wave-59879.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'https://glacial-wave-59879.herokuapp.com', same_site: :none, secure: true
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
